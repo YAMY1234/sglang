42ec178d519f0aa3f1dd82d30bb5ffe229965f96
Ben Barsdell <bbarsdell@nvidia.com>
2025-11-12 03:40:49 +0000

    Use increased workspace size for Llama models
    - Fixes `RuntimeError: Error in function 'aligned_alloc'` due to
      insufficient flashinfer workspace size.
    - Note that this could also be WAR'd by setting
      SGLANG_FLASHINFER_WORKSPACE_SIZE=536870912

--- python/sglang/srt/layers/attention/flashinfer_backend.py
+++ python/sglang/srt/layers/attention/flashinfer_backend.py
@@ -169,6 +169,7 @@ class FlashInferAttnBackend(AttentionBackend):
             in model_runner.model_config.hf_config.architectures
             or "Qwen3VLMoeForConditionalGeneration"
             in model_runner.model_config.hf_config.architectures
+            or "LlamaForCausalLM" in model_runner.model_config.hf_config.architectures
         ):
             envs.SGLANG_FLASHINFER_WORKSPACE_SIZE.set(512 * 1024 * 1024)
 
