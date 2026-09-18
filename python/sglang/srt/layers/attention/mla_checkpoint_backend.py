65bac84548e3f3b9337df38a765af0d4278700aa
Yangmin Li <yangminl@nvidia.com>
2026-09-18 11:45:44 -0700

    perf: skip latent reconstruction and reductions on wholly native blocks

--- python/sglang/srt/layers/attention/mla_checkpoint_backend.py
+++ python/sglang/srt/layers/attention/mla_checkpoint_backend.py
@@ -34,49 +34,54 @@ def _rope(Z, Idx, Val, Sc, Norm, Pos, Native, Re, Rm, A, CosSin, Rope,
         loc = tl.load(Req + row*ROW + start+t, start+t < length, 0).to(tl.int64)
         native = tl.load(Native+loc)
         valid = (start+t < length) & (native < 0)
-        pe = tl.full((16, 64), 0., tl.float32)
-        for kstart in range(0, R, 128):
-            r = kstart+rk
-            z = tl.load(Z+loc[:, None]*R+r[None, :], valid[:, None] & (r[None, :] < R), 0).to(tl.float32)
-            re = tl.load(Re+d[None, :]*R+r[:, None], r[:, None] < R, 0)
-            pe += tl.dot(z, re, input_precision="tf32x3")
-        pe += tl.load(Rm+d)[None, :]
-        if SP > 0:
-            for s in range(0, SP, 8):
-                ss = s+tl.arange(0, 8)
-                ix = _sparse_idx(Idx, loc, ss, SP)
-                val = tl.load(Val+loc[:, None]*SP+ss[None, :]).to(tl.float8e4nv, bitcast=True).to(tl.float32)
-                w = tl.load(A+(512+d[None, None, :])*2048+ix[:, :, None])
-                pe += tl.sum(w*val[:, :, None], 1)*tl.load(Sc+loc)[:, None]
-        pe /= tl.load(Norm+loc*NS)[:, None]
-        pos = tl.load(Pos+loc).to(tl.int64)
-        cos = tl.load(CosSin+pos[:, None]*64+(d[None, :]//2))
-        sin = tl.load(CosSin+pos[:, None]*64+32+(d[None, :]//2))
-        paired = tl.gather(pe, tl.broadcast_to((d ^ 1)[None, :], (16, 64)), 1)
-        pe = pe*cos + tl.where(d[None, :] % 2 == 0, -paired, paired)*sin
-        tl.store(Rope+loc[:, None]*64+d[None, :], pe, valid[:, None])
+        if tl.sum(valid.to(tl.int32), 0) > 0:
+            pe = tl.full((16, 64), 0., tl.float32)
+            for kstart in range(0, R, 128):
+                r = kstart+rk
+                z = tl.load(Z+loc[:, None]*R+r[None, :], valid[:, None] & (r[None, :] < R), 0).to(tl.float32)
+                re = tl.load(Re+d[None, :]*R+r[:, None], r[:, None] < R, 0)
+                pe += tl.dot(z, re, input_precision="tf32x3")
+            pe += tl.load(Rm+d)[None, :]
+            if SP > 0:
+                for s in range(0, SP, 8):
+                    ss = s+tl.arange(0, 8)
+                    ix = _sparse_idx(Idx, loc, ss, SP)
+                    val = tl.load(Val+loc[:, None]*SP+ss[None, :]).to(tl.float8e4nv, bitcast=True).to(tl.float32)
+                    w = tl.load(A+(512+d[None, None, :])*2048+ix[:, :, None])
+                    pe += tl.sum(w*val[:, :, None], 1)*tl.load(Sc+loc)[:, None]
+            pe /= tl.load(Norm+loc*NS)[:, None]
+            pos = tl.load(Pos+loc).to(tl.int64)
+            cos = tl.load(CosSin+pos[:, None]*64+(d[None, :]//2))
+            sin = tl.load(CosSin+pos[:, None]*64+32+(d[None, :]//2))
+            paired = tl.gather(pe, tl.broadcast_to((d ^ 1)[None, :], (16, 64)), 1)
+            pe = pe*cos + tl.where(d[None, :] % 2 == 0, -paired, paired)*sin
+            tl.store(Rope+loc[:, None]*64+d[None, :], pe, valid[:, None])
 
 
 @tr.jit
 def _scores(Q, Qz, Qh, Bias, Z, Idx, Val, Sc, Norm, Native, KV, Rope,
             loc, valid, bh, R: tl.constexpr, SP: tl.constexpr, NS: tl.constexpr,
-            L: tl.constexpr, BR: tl.constexpr, BS: tl.constexpr, SCALE: tl.constexpr):
+            L, BR: tl.constexpr, BS: tl.constexpr, SCALE: tl.constexpr):
     native = tl.load(Native+loc)
     compact = valid & (native < 0)
     nr = tl.maximum(native, 0)
     r = tl.arange(0, BR)
-    z = tl.load(Z+loc[:, None]*R+r[None, :], compact[:, None] & (r[None, :] < R), 0).to(tl.float32)
-    qz = tl.load(Qz+bh*R+r, r < R, 0)
-    score = tl.sum(z*qz[None, :], 1)+tl.load(Bias+bh)
-    if SP > 0:
-        ss = tl.arange(0, BS)
-        # SP supported in multiples of powers of two for this kernel.
-        ix = _sparse_idx(Idx, loc, tl.minimum(ss, SP-1), SP)
-        val = tl.load(Val+loc[:, None]*SP+ss[None, :], ss[None, :] < SP, 0).to(tl.float8e4nv, bitcast=True).to(tl.float32)
-        qh = tl.load(Qh+bh*2048+ix)
-        score += tl.sum(qh*val, 1)*tl.load(Sc+loc)
-    norm = tl.load(Norm+loc*NS)*tl.load(Norm+loc*NS+L)
-    score /= norm
+    z = tl.full((16, BR), 0., tl.float32)
+    score = tl.full((16,), 0., tl.float32)
+    norm = tl.full((16,), 1., tl.float32)
+    if tl.sum(compact.to(tl.int32), 0) > 0:
+        z = tl.load(Z+loc[:, None]*R+r[None, :], compact[:, None] & (r[None, :] < R), 0).to(tl.float32)
+        qz = tl.load(Qz+bh*R+r, r < R, 0)
+        score = tl.sum(z*qz[None, :], 1)+tl.load(Bias+bh)
+        if SP > 0:
+            ss = tl.arange(0, BS)
+            # SP supported in multiples of powers of two for this kernel.
+            ix = _sparse_idx(Idx, loc, tl.minimum(ss, SP-1), SP)
+            val = tl.load(Val+loc[:, None]*SP+ss[None, :], ss[None, :] < SP, 0).to(tl.float8e4nv, bitcast=True).to(tl.float32)
+            qh = tl.load(Qh+bh*2048+ix)
+            score += tl.sum(qh*val, 1)*tl.load(Sc+loc)
+        norm = tl.load(Norm+loc*NS)*tl.load(Norm+loc*NS+L)
+        score /= norm
     d = tl.arange(0, 512)
     c = tl.load(KV+nr[:, None]*576+d[None, :], valid[:, None] & ~compact[:, None], 0).to(tl.float32)
     qc = tl.load(Q+bh*576+d).to(tl.float32)
@@ -92,7 +97,7 @@ def _scores(Q, Qz, Qh, Bias, Z, Idx, Val, Sc, Norm, Native, KV, Rope,
 @tr.jit
 def _lse(Q, Qz, Qh, Bias, Z, Idx, Val, Sc, Norm, Native, KV, Rope,
          Req, Rows, Lens, Part, ROW: tl.constexpr, HEADS: tl.constexpr,
-         R: tl.constexpr, SP: tl.constexpr, NS: tl.constexpr, L: tl.constexpr,
+         R: tl.constexpr, SP: tl.constexpr, NS: tl.constexpr, L,
          BR: tl.constexpr, BS: tl.constexpr, SPLITS: tl.constexpr, SCALE: tl.constexpr):
     bh, split = tl.program_id(0), tl.program_id(1)
     b = bh//HEADS
@@ -115,7 +120,7 @@ def _lse(Q, Qz, Qh, Bias, Z, Idx, Val, Sc, Norm, Native, KV, Rope,
 @tr.jit
 def _output(Q, Qz, Qh, Bias, Z, Idx, Val, Sc, Norm, Native, KV, Rope,
             Req, Rows, Lens, Part, Uz, Uc, Uh, Mass, ROW: tl.constexpr, HEADS: tl.constexpr,
-            R: tl.constexpr, SP: tl.constexpr, NS: tl.constexpr, L: tl.constexpr,
+            R: tl.constexpr, SP: tl.constexpr, NS: tl.constexpr, L,
             BR: tl.constexpr, BS: tl.constexpr, SPLITS: tl.constexpr, SCALE: tl.constexpr):
     bh, split = tl.program_id(0), tl.program_id(1)
     b = bh//HEADS
@@ -139,15 +144,16 @@ def _output(Q, Qz, Qh, Bias, Z, Idx, Val, Sc, Norm, Native, KV, Rope,
             loc,valid,bh,R,SP,NS,L,BR,BS,SCALE)
         p = tl.exp(scores-logsum)
         ps = tl.where(compact, p/norm, 0.)
-        uz += tl.sum(ps[:, None]*z, 0)
         uc += tl.sum(p[:, None]*c, 0)
-        mass += tl.sum(ps, 0)
-        if SP > 0:
-            ss = tl.arange(0, BS)
-            ix = _sparse_idx(Idx, loc, tl.minimum(ss, SP-1), SP)
-            val = tl.load(Val+loc[:, None]*SP+ss[None, :], ss[None, :] < SP, 0).to(tl.float8e4nv, bitcast=True).to(tl.float32)
-            value = (ps*tl.load(Sc+loc))[:, None]*val
-            tl.atomic_add(Uh+bh*2048+ix, value, compact[:, None] & (ss[None, :] < SP), sem="relaxed")
+        if tl.sum(compact.to(tl.int32), 0) > 0:
+            uz += tl.sum(ps[:, None]*z, 0)
+            mass += tl.sum(ps, 0)
+            if SP > 0:
+                ss = tl.arange(0, BS)
+                ix = _sparse_idx(Idx, loc, tl.minimum(ss, SP-1), SP)
+                val = tl.load(Val+loc[:, None]*SP+ss[None, :], ss[None, :] < SP, 0).to(tl.float8e4nv, bitcast=True).to(tl.float32)
+                value = (ps*tl.load(Sc+loc))[:, None]*val
+                tl.atomic_add(Uh+bh*2048+ix, value, compact[:, None] & (ss[None, :] < SP), sem="relaxed")
     tl.store(Uz+(bh*SPLITS+split)*R+r, uz, r < R)
     tl.store(Uc+(bh*SPLITS+split)*512+d, uc)
     tl.store(Mass+bh*SPLITS+split, mass)
